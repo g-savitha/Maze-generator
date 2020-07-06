97dd59f408f7c7fb8fd5ee455c9d423b25782060
Savitha <savitha.gollamudi@gmail.com>
2020-07-06 12:39:15 +0530

    grid generation

--- index.js
+++ index.js
@@ -28,3 +28,32 @@ const walls = [
 ];
 
 World.add(world, walls);
+
+//Maze generation
+
+// [
+//   [false, false, false],
+//   [false, false, false],
+//   [false, false, false],
+// ];
+
+const grid = Array(3)
+  .fill(null)
+  .map(() => Array(3).fill(false));
+
+//why do we need to use map? why d\cant we direct fill([false,false,false])
+// grid
+// (3) [Array(3), Array(3), Array(3)]
+// 0: (3) [false, false, false]
+// 1: (3) [false, false, false]
+// 2: (3) [false, false, false]
+
+// grid[0].push(true)
+// grid
+
+// (3) [Array(4), Array(4), Array(4)]
+// 0: (4) [false, false, false, true]
+// 1: (4) [false, false, false, true]
+// 2: (4) [false, false, false, true]
+
+//using map (.map(() => Array(3).fill(false))), each time we generate a brand new array.
